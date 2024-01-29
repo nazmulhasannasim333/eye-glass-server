58d011aad85fab9f862a69dc84d3499c3beb8ff4
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-01-29 15:57:39 +0600

    change origin url in app.tsx

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ const app: Application = express();
 
 // parsers
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["https://eye-glass-inventory.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Server Running on port ${config.port}`);
